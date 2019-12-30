dd5061d53ec755198588e470b1e26b253b74452c
Priyanshu <psingh1002@gmail.com>
2019-12-30 15:52:09 +0530

    helper

---
+++ helper.py
@@ -0,0 +1,59 @@
+import sqlite3 as lite
+
+
+#functionality goes here
+
+class DatabaseManage(object):
+
+	def __init__(self):
+		global con
+		try:
+			con = lite.connect('courses.db')
+			with con:
+				cur = con.cursor()
+				cur.execute("CREATE TABLE IF NOT EXISTS course(Id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, price TEXT, is_private BOOLEAN NOT NULL DEFAULT 1)")
+			except Exception(exc):
+				print("Unable to create a DB!")
+
+#TODO: create data
+def insert_data(self, data):
+	try:
+		with con:
+			cur = con.cursor()
+			cur.execute(
+				"INSERT INTO course(name, description, price, is_private) VALUES(?,?,?,?)", data
+				)
+	except Exception:
+		return False
+
+#TODO: read data
+def fetch_data(self):
+	try:
+		with con:
+			cur = con.cursor()
+			cur.execute("SELECT * FROM course")
+			return cur.fetchall()
+	except Exception:
+		return False
+
+#TODO: delete data
+def delete_data(self, id):
+	try:
+		with con:
+			cur = con.cursor()
+			sql = "DELETE FROM course WHERE id = ?"
+			cur.execute(sql, [id])
+
+	except Exception:
+		return False
+
+#TODO: provide interface to user
+
+def main():
+	print("*\n"*40)
+	print("\n:: COURSE MANAGEMENT :: \n")
+	print("*\n"*40)
+
+	db = DatabaseManage()
+
+	
